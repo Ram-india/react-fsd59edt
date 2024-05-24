6af6f46d770e5f384ad57717b111c8c35f8b5542
Ram-india <bhv.ramece@gmail.com>
2024-05-24 12:09:36 +0530

    Class Component creation

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+
+class App extends Component{
+  //override the render method
+  render(){
+    //return JSX
+    return(
+      <div>
+        <h1>Class Component</h1>
+      </div>
+    )
+  }
+}
+export default App;
